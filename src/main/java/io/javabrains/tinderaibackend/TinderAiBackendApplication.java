4a6c2c0a8e33cfebddd5611edc4ca7df61ac6f5d
Suffian <suffianakhtar@gmail.com>
2026-02-22 14:49:13 +0500

    Add a Profile to database and fetch it successfully

--- src/main/java/io/javabrains/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/io/javabrains/tinderaibackend/TinderAiBackendApplication.java
@@ -1,13 +1,30 @@
 package io.javabrains.tinderaibackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.javabrains.tinderaibackend.profiles.Gender;
+import io.javabrains.tinderaibackend.profiles.Profile;
+import io.javabrains.tinderaibackend.profiles.ProfileRepository;
+
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		Profile profile = new Profile("1", "Sheifu", "Saif", 30, "Paki", Gender.MALE, "Software Engineer", "foo.jpg",
+				"INTP");
+		profileRepository.save(profile);
+		
+		profileRepository.findAll().forEach(System.out::println);
 	}
 
 }
---
+++ src/main/java/io/javabrains/tinderaibackend/profiles/ProfileRepository.java
@@ -0,0 +1,7 @@
+package io.javabrains.tinderaibackend.profiles;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProfileRepository extends MongoRepository<Profile, String> {
+	
+}
