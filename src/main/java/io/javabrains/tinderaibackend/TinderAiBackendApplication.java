f1acff90c8e787f27cb898dde4ec37a00e4e65c8
Suffian <suffianakhtar@gmail.com>
2026-02-22 19:33:23 +0500

    Add ProfileCreationService and moving from testing to production work

--- src/main/java/io/javabrains/tinderaibackend/TinderAiBackendApplication.java
+++ src/main/java/io/javabrains/tinderaibackend/TinderAiBackendApplication.java
@@ -1,26 +1,17 @@
 package io.javabrains.tinderaibackend;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.javabrains.tinderaibackend.conversations.ChatMessage;
-import io.javabrains.tinderaibackend.conversations.Conversation;
-import io.javabrains.tinderaibackend.conversations.ConversationRepository;
-import io.javabrains.tinderaibackend.profiles.Gender;
-import io.javabrains.tinderaibackend.profiles.Profile;
-import io.javabrains.tinderaibackend.profiles.ProfileRepository;
+import io.javabrains.tinderaibackend.profiles.ProfileCreationService;
 
 @SpringBootApplication
 public class TinderAiBackendApplication implements CommandLineRunner {
 	@Autowired
-	private ProfileRepository profileRepository;
-	@Autowired
-	private ConversationRepository conversationRepository;
+	private ProfileCreationService profileCreationService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
@@ -28,21 +19,7 @@ public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		// Clearing existing profiles and conversations
-		profileRepository.deleteAll();
-		conversationRepository.deleteAll();
-		
-		Profile profile = new Profile("1", "Sheifu", "Saif", 30, "Paki", Gender.MALE, "Software Engineer", "foo.jpg",
-				"INTP");
-		profileRepository.save(profile);
-		profile = new Profile("2", "Foo", "Bar", 35, "Irani", Gender.FEMALE, "Cook", "foo.jpg", "INTP");
-		profileRepository.save(profile);
-		profileRepository.findAll().forEach(System.out::println);
-
-		Conversation conversation = new Conversation("1", profile.id(),
-				List.of(new ChatMessage("Hello", profile.id(), LocalDateTime.now())));
-		conversationRepository.save(conversation);
-		conversationRepository.findAll().forEach(System.out::println);
+		profileCreationService.saveProfilesToDb();
 	}
 
 }
---
+++ src/main/java/io/javabrains/tinderaibackend/profiles/ProfileCreationService.java
@@ -0,0 +1,50 @@
+package io.javabrains.tinderaibackend.profiles;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@Service
+public class ProfileCreationService {
+	private static final String PROFILES_FILE_PATH = "profiles.json";
+	
+	@Value("#{${tinderai.character.user}}")
+	private Map<String, String> userProfileProperties;
+	
+	@Autowired
+	private ProfileRepository profileRepository;
+
+	public void saveProfilesToDb() {
+		Gson gson = new Gson();
+		
+		try {
+			List<Profile> existingProfiles = gson.fromJson(new FileReader(PROFILES_FILE_PATH), new TypeToken<ArrayList<Profile>>() {}.getType());
+			profileRepository.deleteAll();
+			profileRepository.saveAll(existingProfiles);
+		} catch(FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		Profile profile = new Profile(
+				userProfileProperties.get("id"),
+				userProfileProperties.get("firstName"),
+				userProfileProperties.get("lastName"),
+				Integer.parseInt(userProfileProperties.get("age")),
+				userProfileProperties.get("ethnicity"),
+				Gender.valueOf(userProfileProperties.get("gender")),
+				userProfileProperties.get("bio"),
+				userProfileProperties.get("imageUrl"),
+				userProfileProperties.get("myersBriggsPersonalityType")
+		);
+		System.out.println(userProfileProperties);
+		profileRepository.save(profile);
+	}
+}
